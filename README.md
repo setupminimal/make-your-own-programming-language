88fcf5c624d97ebebc85379892ffde9147ab7dae
Daroc Alden <setupminimal@gmail.com>
2020-10-24 08:04:56 -0400

    Basic parsing, interpretation, and addition
    This commit adds the LICENSE and README.md files, but more importantly,
    it also adds the first, most basic interpreter that we'll create.
    
    Since we're starting from scratch, and not building on other commits
    the way that later versions will, it's a big chunk of code to digest.
    If you can understand what's going on here, you'll have no trouble
    following the rest of the code that we develop. If you are a bit
    confused about what exactly the code in this commit does, there
    are a few  things you should do:
    
    1) Read other resources to explain any terms you don't understand
    2) Play with the code, to see how it breaks when you change stuff
    3) Open an issue with your question on the GitHub repository: [1]
    
    [1]: https://github.com/setupminimal/myoplitmol

---
+++ README.md
@@ -0,0 +1,18 @@
+# Make Your Own Programming Language in 30 Minutes
+
+... is the title of a talk that I gave, which is about exactly
+what it sounds like. This repository contains the written version -
+each commit is a working version of the code that I build up in that talk
+(and a little bit more, because I'm not constrained by time here), liberally
+commented. Hopefully this will be useful and/or interesting to someone.
+
+The commit messages are also very detailed, so be sure to read those as well.
+
+Everything in this repository is available under an MIT license - see `LICENSE`
+
+## Contributing
+
+Contributions are absolutely welcome! Since this repository uses the commits
+and commit messages to break the program down into easy, manageable steps,
+please don't just submit pull requests. Send in patches for the commit(s) you
+want to amend, and I'll rebase the repository to include them.
---
+++ main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of the supplementary materials
+for a talk called "Make Your Own Programming Language in 30 Minutes".
+
+The git repository that contains this file starts from scratch, and
+slowly builds its way up towards a working interpreter for a version
+of Forth with several additional features meant to highlight how to
+extend the basic language. The commit messages also explain what is
+being done in each step and why, so be sure to take a look at those
+
+The whole point is to get you excited about making your own programming
+language - please grab a text editor, your programming language of choice,
+and follow along! All the code in this repository is under a MIT license,
+meaning that you can copy it, adapt it, and learn from it, freely and
+without restriction, as long as you credit me (Daroc Alden) in any
+derivative works.
+
+With that said, let's dive right in.
+"""
+
+import sys
+
+# The program that we are to interpret should
+# be passed in as the first and only command-line
+# argument. We will tidy this up to provide a
+# helpful error message later, after we've gotten
+# the core language working.
+with open(sys.argv[1], 'r') as f:
+    # We're basing the language that we're creating here on Forth[1] for a very
+    # simple reason. Most programming languages have some amount of syntax
+    # that we would have to tokenize and parse. Forth-like languages have
+    # basically no syntax at all - programs are just lists of words, separated
+    # by whitespace.
+    #
+    # [1]: https://en.wikipedia.org/wiki/Forth_(programming_language)
+    #
+    # You can think of this step as the 'tokenization' step that a lot of other
+    # programming languages need. The only real difference that since our
+    # syntax is so simple, the code to split the program into tokens is also
+    # really simple. If you wanted to, you could later extend this step
+    # to do more complicated processing (for example, to handle parentheses
+    # differently, so that they don't just get counted as part of the word
+    # that they touch.)
+    program = f.read().split()
+
+# We're later going to treat the source code of the program like a stack,
+# which means frequently popping from and appending to the source. That's
+# more efficient (in Python at least, which uses Array-backed lists) to do
+# from the end, so we reverse the program, so that the first word is at the end
+#
+# This is like the 'parsing' step in other programming languages: We're
+# changing the representation of the program into something that is easier for
+# our interpreter to deal with. In this case, since Forth has very little
+# syntax, that's quite easy. You could extend this step to do more complicated
+# parsing, for example to implement list literals. We'll add more complexity
+# here in future commits.
+program.reverse()
+
+# This will be used momentarily in our 'interpret' function.
+# `builtins` is used to represent the basic functions that are defined
+# in python.
+builtins = {}
+
+def interpret(program, stack=[]):
+    """
+    Interpret takes a program (with the first word last, as discussed above)
+    and an optional stack of data to use, and performs whatever function
+    the program describes upon the stack. The new stack is returned.
+    """
+    
+    while program:
+        # This removes the 'next' word in the program
+        word = program.pop()
+
+        # If the word is a built-in word,
+        # we just look up its definition and call the python
+        # code that implements it. Notice how we pass in both
+        # the program and the stack - that will be important
+        # later, because it allows builtin words to modify the
+        # remaining program.
+        if word in builtins.keys():
+            builtins[word](program, stack)
+        # If the word hasn't been defined, we check to see if
+        # it's a constant, like `2`. If so, the data just gets
+        # put on the stack.
+        elif word.isnumeric():
+            stack.append(int(word))
+        # And if the word wasn't recognized, we emit an error
+        # message and quit. We'll revise this later to add
+        # a feature like Ruby's `method_missing` that can
+        # do something more helpful.
+        else:
+            print(f'Unknown word {word}')
+            sys.exit(1)
+    
+    return stack
+
+# Finally, we need at least one builtin in order to have a program
+# _do_ anything. This code just pops two numbers off the stack, adds
+# them together, and puts the result back on the stack.
+builtins['+'] = lambda p, stack: stack.append(stack.pop() + stack.pop())
+
+# So now we can just show the results of running our program. This would
+# be a great time to go look at example1.ql and make sure that your version
+# of the code works the same, and that you understand what it's doing.
+print(interpret(program))
